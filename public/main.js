d5bc05863d81fa7719ab7af0c959d23e050790c2
ravl17 <ravlgg511@gmail.com>
2023-11-10 13:07:33 +0100

    wip/refactor code

--- public/main.js
+++ public/main.js
@@ -1,39 +1,35 @@
 var pulsado = false;
 
-function crearPixel(tableX, tableY) {
-    //Falta cambiar nombre variables y cambiar X por Y jeje
-    var divPixel = document.getElementById("tabl");
-    divPixel.addEventListener("mousedown", botonPulsado);
-    divPixel.addEventListener("mouseup", botonSoltado);
+function crearPixel() {
+    let matrixWidth = document.getElementById("matrixWidth").value;
+    let matrixHeight = document.getElementById("matrixHeight").value;
 
+    var matrixLed = document.getElementById("matrixLed");
+    matrixLed.addEventListener("mousedown", botonPulsado);
+    matrixLed.addEventListener("mouseup", botonSoltado);
 
-    document.getElementById("tabl").innerHTML = "";
-    var tab = document.getElementById("tabl");
+    document.getElementById("matrixLed").innerHTML = "";
 
-    for (i = 0; i < tableY; i++) {
-        var trtr = document.createElement("tr");
+    for (i = 0; i < matrixHeight; i++) {
+        var tr = document.createElement("tr");
 
-        for (j = 0; j < tableX; j++) {
-            var a = document.createElement("td");
-            a.setAttribute("id", "led_" + j + "_" + i);
-
+        for (j = 0; j < matrixWidth; j++) {
+            var td = document.createElement("td");
             var ledColor = document.createElement("BUTTON");
             ledColor.setAttribute("id", "buttonLedColor" + j + "_" + i);
+            ledColor.setAttribute("class", "button_led");
             ledColor.addEventListener('mouseover', function onClick(event) {
                 if (pulsado) {
                     event.target.style.backgroundColor = document.getElementById("colorSeleccionado").value;
                 }
-
             });
             ledColor.addEventListener('click', function onClick(event) {
                 event.target.style.backgroundColor = document.getElementById("colorSeleccionado").value;
-
             });
-            a.appendChild(ledColor);
-            // arr.push(a);
-            trtr.appendChild(a);
+            td.appendChild(ledColor);
+            tr.appendChild(td);
         }
-        tab.appendChild(trtr);
+        matrixLed.appendChild(tr);
     }
 }
 function botonPulsado() {
@@ -43,8 +39,11 @@ function botonPulsado() {
 function botonSoltado() {
     pulsado = false;
 }
-function sendData(tableX, tableY) {
-    const bitmap = createBitmap(tableX, tableY);
+function sendData() {
+    let matrixWidth = document.getElementById("matrixWidth").value;
+    let matrixHeight = document.getElementById("matrixHeight").value;
+
+    const bitmap = createBitmap(matrixWidth, matrixHeight);
     fetch('/submit-data', {
         method: 'POST',
         headers: {
@@ -55,24 +54,18 @@ function sendData(tableX, tableY) {
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            // You can handle the server response here.
         })
         .catch(error => console.error('Error:', error));
 }
-function createBitmap(tableX, tableY) {
-    const bitmap = new Array();
-    for (let i = 0; i < tableY; i++) {
-        for (let j = 0; j < tableX; j++) {
+function createBitmap(matrixWidth, matrixHeight) {
+    const uint16Array = new Uint16Array(matrixWidth * matrixHeight);
+    for (let i = 0; i < matrixHeight; i++) {
+        for (let j = 0; j < matrixWidth; j++) {
             const cell = document.getElementById("buttonLedColor" + j + "_" + i);
             const color = cell.style.backgroundColor;
             const rgb565 = rgbToRgb565(color);
-            bitmap.push(rgb565);
+            uint16Array[matrixWidth * i + j] = rgb565ToUint16(rgb565);
         }
-    }
-    const uint16Array = new Uint16Array(bitmap.length);
-    for (let i = 0; i < bitmap.length; i++) {
-        const color = bitmap[i];
-        uint16Array[i] = rgb565ToUint16(color);
     }
 
     return uint16Array;
@@ -88,26 +81,17 @@ function rgbToRgb565(color) {
         red = Math.round(red / 255 * 31);
         green = Math.round(green / 255 * 63);
         blue = Math.round(blue / 255 * 31);
-
         const rgb565Value = (red << 11) | (green << 5) | (blue);
         const rgb565Hex = rgb565Value.toString(16).toUpperCase();
-        return "0x" + rgb565Hex;
+        return rgb565Hex;
     } else {
-        return "0x0";
+        return "0";
     }
 }
 
 
-
-
-
-
 function rgb565ToUint16(hexString) {
     try {
-        // Remove the '0x' prefix if it exists
-        if (hexString.startsWith("0x")) {
-            hexString = hexString.slice(2);
-        }
         // Convert the hexadecimal string to a binary string
         let binaryString = (parseInt(hexString, 16) >>> 0);
 
@@ -116,4 +100,11 @@ function rgb565ToUint16(hexString) {
         console.log(error);
         return null;
     }
+}
+
+function fillMatrix() {
+    let leds = document.querySelectorAll('.button_led');
+    leds.forEach(led => {
+        led.style.backgroundColor = document.getElementById("colorSeleccionado").value;
+    })
 }
